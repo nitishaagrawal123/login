a067f5969c8553de59a8ca7b18e6aa0d872c0a92
nitishaagrawal123 <47311849+nitishaagrawal123@users.noreply.github.com>
2019-03-22 09:53:12 +0530

    Add files via upload

---
+++ JPA Inheritance Project 1/src/Manager.java
@@ -0,0 +1,32 @@
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="mgr_store")
+public class Manager extends Emp {
+	private String deptName;
+
+	public Manager() {
+		super();
+	}
+
+	public Manager(int empId, String empName, float empSal,String deptName) {
+		super(empId, empName, empSal);
+		this.deptName=deptName;
+	}
+	
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+	@Override
+	public String toString() {
+		return "Manager [deptName=" + deptName + "]";
+	}
+
+}
